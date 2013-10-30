fa6695a5e5f15ef95c4ce4c35dfa013f2297b972
Adrian Kuendig <adriankue@gmail.com>
2013-10-30 22:55:34 +0100

    Removed pointers

--- src/peg.rs
+++ src/peg.rs
@@ -73,14 +73,6 @@ impl<'self> Eq for StringStream<'self> {
 	}
 }
 
-// #[deriving(Eq)]
-// pub enum ParseResult<T> {
-// 	Success(T, ~StringStream),
-// 	Failure(~str),
-// }
-
-// pub type Parser<'self, T> = &'self fn<'r>(StringStream<'r>) -> ParseResult<'r, T>;
-
 mod parser {
 	use super::{StringStream};
 
@@ -92,142 +84,211 @@ mod parser {
 		fn run_string_stream<'r>(&self, input: StringStream<'r>) -> ParseResult<'r, T>;
 	}
 
-	pub type ParseResult<'self, T> = Result<(T, StringStream<'self>), ~str>;
+	// pub type ParseResult<'self, T> = Result<(T, StringStream<'self>), ~str>;
+	#[deriving(Eq, ToStr)]
+	pub enum ParseResult<'self, T> {
+	    Success(T, StringStream<'self>),
+	    Failure(~str),
+	}
 
-	struct Success;
-
-	impl Parser<()> for Success {
-		fn run_string_stream<'r>(&self, input: StringStream<'r>) -> ParseResult<'r, ()> {
-			Ok(((), input))
+	impl<'self, T> ParseResult<'self, T> {
+		pub fn get(self) -> T {
+			match self {
+				Success(v, _) => v,
+				Failure(err) => fail!(format!("parser::ParseResult::get Failure({})", err)),
+			}
 		}
-	}
 
-	pub fn success() -> ~Parser<()> {
-		~Success as ~Parser<()>
-	}
+		pub fn get_err(self) -> ~str {
+			match self {
+				Success(v, rest) => fail!(format!("parser::ParseResult::get Success({:?}, {:?})", v, rest)),
+				Failure(err) => err.clone(),
+			}
+		}
 
-	struct Peek {
-		n: int
-	}
+		pub fn unwrap(self) -> (T, StringStream<'self>) {
+			match self {
+				Success(v, rest) => (v, rest),
+				Failure(err) => fail!(format!("parser::ParseResult::get Failure({})", err)),
+			}
+		}
 
-	impl Parser<char> for Peek {
-		fn run_string_stream<'r>(&self, input: StringStream<'r>) -> ParseResult<'r, char> {
-			match input.head() {
-				Some((c, inp)) => Ok((c, inp)),
-				None => Err(format!("Could not match char because EOI.")),
+		pub fn map<U>(self, f: &fn(v: &T) -> U) -> ParseResult<'self, U> {
+			match self {
+				Success(v, rest) => Success(f(&v), rest),
+				Failure(err) => Failure(err),
 			}
 		}
 	}
 
-	pub fn peek() -> ~Parser<char> {
-		~Peek{ n: 1 } as ~Parser<char>
+	struct Accept;
+
+	impl Parser<()> for Accept {
+		fn run_string_stream<'r>(&self, input: StringStream<'r>) -> ParseResult<'r, ()> {
+			Success((), input)
+		}
+	}
+
+	pub fn success() -> Accept {
+		Accept
+	}
+
+	pub struct Peek;
+
+	impl Parser<char> for Peek {
+		fn run_string_stream<'r>(&self, input: StringStream<'r>) -> ParseResult<'r, char> {
+			match input.head() {
+				Some((c, inp)) => Success(c, inp),
+				None => Failure(format!("Could not match char because EOI.")),
+			}
+		}
+	}
+
+	pub fn peek() -> Peek {
+		Peek
 	}
 
 	struct Char {
-		c: char
+		priv c: char
 	}
 
 	impl Parser<char> for Char {
 		fn run_string_stream<'r>(&self, input: StringStream<'r>) -> ParseResult<'r, char> {
 			match input.head() {
 				Some((head, inp)) =>
-					if head == self.c { Ok((self.c, inp)) }
-					else { Err(format!("Could not find matching char. found: '{}' required: '{}'", head, self.c)) },
-				None => Err(format!("Could not match char '{}' because EOI.", self.c)),
+					if head == self.c { Success(self.c, inp) }
+					else { Failure(format!("Could not find matching char. found: '{}' required: '{}'", head, self.c)) },
+				None => Failure(format!("Could not match char '{}' because EOI.", self.c)),
 			}
 		}
 	}
 
-	pub fn chr(c: char) -> ~Parser<char> {
-		~Char{ c: c } as ~Parser<char>
+	pub fn chr(c: char) -> Char {
+		Char{ c: c }
 	}
 
 	struct Text {
-		txt: ~str
+		priv txt: ~str
 	}
 
 	impl Parser<~str> for Text {
 		fn run_string_stream<'r>(&self, input: StringStream<'r>) -> ParseResult<'r, ~str> {
 			match input.take(self.txt.char_len()) {
 				Some((t, inp)) =>
-					if t == self.txt { Ok((t, inp)) }
-					else { Err(format!("Could not find matching string. found: \"{}\" required: \"{}\"", t, self.txt)) },
-				None => Err(format!("Could not match string \"{}\" because EOI.", self.txt)),
+					if t == self.txt { Success(t, inp) }
+					else { Failure(format!("Could not find matching string. found: \"{}\" required: \"{}\"", t, self.txt)) },
+				None => Failure(format!("Could not match string \"{}\" because EOI.", self.txt)),
 			}
 		}
 	}
 
-	pub fn text(txt: ~str) -> ~Parser<~str> {
-		~Text{ txt: txt } as ~Parser<~str>
+	pub fn text(txt: ~str) -> Text {
+		Text{ txt: txt }
 	}
 
 	struct AnyOf {
-		valid: ~[char]
+		priv valid: ~[char]
 	}
 
 	impl Parser<char> for AnyOf {
 		fn run_string_stream<'r>(&self, input: StringStream<'r>) -> ParseResult<'r, char> {
 			match input.head() {
-				Some((c, inp)) if self.valid.contains(&c) => Ok((c, inp)),
-				Some((c, _)) => Err(format!("Could not find matching char. found: '{}' required any of: {:?}", c, self.valid)),
-				None => Err(format!("Could not match any of {:?} because EOI.", self.valid)),
+				Some((c, inp)) if self.valid.contains(&c) => Success(c, inp),
+				Some((c, _)) => Failure(format!("Could not find matching char. found: '{}' required any of: {:?}", c, self.valid)),
+				None => Failure(format!("Could not match any of {:?} because EOI.", self.valid)),
 			}
 		}
 	}
 
-	pub fn anyOf(valid: ~[char]) -> ~Parser<char> {
-		~AnyOf{ valid: valid } as ~Parser<char>
+	pub fn anyOf(valid: ~[char]) -> AnyOf {
+		AnyOf{ valid: valid }
 	}
 
 	struct Or<T, U> {
-		p1: ~Parser<T>,
-		p2: ~Parser<U>,
+		priv p1: T,
+		priv p2: U,
 	}
 
-	impl<T, U> Parser<Either<T, U>> for Or<T, U> {
-		fn run_string_stream<'r>(&self, input: StringStream<'r>) -> ParseResult<'r, Either<T, U>> {
+	impl<A, B, T: Parser<A>, U: Parser<B>> Parser<Either<A, B>> for Or<T, U> {
+		fn run_string_stream<'r>(&self, input: StringStream<'r>) -> ParseResult<'r, Either<A, B>> {
 			match self.p1.run_string_stream(input) {
-				Ok((val, inp)) => return Ok((Left(val), inp)),
-				Err(err1) =>
+				Success(val, inp) => Success(Left(val), inp),
+				Failure(err1) =>
 					match self.p2.run_string_stream(input) {
-						Ok((val, inp)) => return Ok((Right(val), inp)),
-						Err(err2) => return Err(format!("Neither of the parsers matched: \n{}\n{}", err1, err2))
+						Success(val, inp) => Success(Right(val), inp),
+						Failure(err2) => Failure(format!("Neither of the parsers matched: \n{}\n{}", err1, err2))
 					}
 			}
 		}
 	}
 
-	pub fn or<T, U>(p1: ~Parser<T>, p2: ~Parser<U>) -> ~Parser<Either<T, U>> {
-		~Or{ p1: p1, p2: p2 } as ~Parser<Either<T, U>>
+	pub fn or<A, B, T: Parser<A>, U: Parser<B>>(p1: T, p2: U) -> Or<T, U> {
+		Or{ p1: p1, p2: p2 }
 	}
 
 	struct And<T, U> {
-		p1: ~Parser<T>,
-		p2: ~Parser<U>,
+		priv p1: T,
+		priv p2: U,
 	}
 
-	impl<T, U> Parser<(T, U)> for And<T, U> {
-		fn run_string_stream<'r>(&self, input: StringStream<'r>) -> ParseResult<'r, (T, U)> {
+	impl<A, B, T: Parser<A>, U: Parser<B>> Parser<(A, B)> for And<T, U> {
+		fn run_string_stream<'r>(&self, input: StringStream<'r>) -> ParseResult<'r, (A, B)> {
 			match self.p1.run_string_stream(input) {
-				Ok((val1, inp1)) =>
+				Success(val1, inp1) =>
 					match self.p2.run_string_stream(inp1) {
-						Ok((val2, inp2)) => Ok(((val1, val2), inp2)),
-						Err(err) => Err(err)
+						Success(val2, inp2) => Success((val1, val2), inp2),
+						Failure(err) => Failure(err)
 					},
-				Err(err1) => Err(err1),
+				Failure(err1) => Failure(err1),
 			}
 		}
 	}
 
-	pub fn and<T, U>(p1: ~Parser<T>, p2: ~Parser<U>) -> ~Parser<(T, U)> {
-		~And{ p1: p1, p2: p2 } as ~Parser<(T, U)>
+	pub fn and<A, B, T: Parser<A>, U: Parser<B>>(p1: T, p2: U) -> And<T, U> {
+		And{ p1: p1, p2: p2 }
+	}
+
+	struct Map<A, B, T> {
+		p: T,
+		f: ~fn(v: A) -> B,
+	}
+
+	impl<A, B, T: Parser<A>> Parser<B> for Map<A, B, T> {
+		fn run_string_stream<'r>(&self, input: StringStream<'r>) -> ParseResult<'r, B> {
+			match self.p.run_string_stream(input) {
+				Success(val, inp) => Success((self.f)(val), inp),
+				Failure(err) => Failure(err),
+			}
+		}
+	}
+
+	pub fn map<A, B, T: Parser<A>>(p: T, f: ~fn(v: A) -> B) -> Map<A, B, T> {
+		Map{ p: p, f: f }
+	}
+
+	struct FlatMap<A, B, T, U> {
+		p: T,
+		f: ~fn(v: A) -> U,
+	}
+
+	impl<A, B, T: Parser<A>, U: Parser<B>> Parser<B> for FlatMap<A, B, T, U> {
+		fn run_string_stream<'r>(&self, input: StringStream<'r>) -> ParseResult<'r, B> {
+			match self.p.run_string_stream(input) {
+				Success(val, inp) => (self.f)(val).run_string_stream(inp),
+				Failure(err) => Failure(err),
+			}
+		}
+	}
+
+	pub fn flatMap<A, B, T: Parser<A>, U: Parser<B>>(p: T, f: ~fn(v: A) -> U) -> FlatMap<A, B, T, U> {
+		FlatMap{ p: p, f: f }
 	}
 }
 
 #[cfg(test)]
 mod test {
 	use super::StringStream;
-	use parser::{success, peek, chr, text, anyOf, or};
+	use parser::{success, peek, chr, text, anyOf, or, map, flatMap, Parser};
 
 
 	fn input() -> StringStream {
@@ -282,7 +343,7 @@ mod test {
 
 		let failure_eof = peek().run_string("");
 
-		assert_eq!(failure_eof, Err(~"Could not match char because EOI."));
+		assert_eq!(failure_eof.get_err(), ~"Could not match char because EOI.");
 	}
 
 	#[test]
@@ -294,11 +355,11 @@ mod test {
 
 		let failure_nomatch = chr('b').run_string("abc");
 
-		assert_eq!(failure_nomatch, Err(~"Could not find matching char. found: 'a' required: 'b'"));
+		assert_eq!(failure_nomatch.get_err(), ~"Could not find matching char. found: 'a' required: 'b'");
 
 		let failure_eof = chr('a').run_string("");
 
-		assert_eq!(failure_eof, Err(~"Could not match char 'a' because EOI."));
+		assert_eq!(failure_eof.get_err(), ~"Could not match char 'a' because EOI.");
 	}
 
 	#[test]
@@ -310,11 +371,11 @@ mod test {
 
 		let failure_nomatch = text(~"af").run_string("abc");
 
-		assert_eq!(failure_nomatch, Err(~"Could not find matching string. found: \"ab\" required: \"af\""));
+		assert_eq!(failure_nomatch.get_err(), ~"Could not find matching string. found: \"ab\" required: \"af\"");
 
 		let failure_eof = text(~"ab").run_string("");
 
-		assert_eq!(failure_eof, Err(~"Could not match string \"ab\" because EOI."));
+		assert_eq!(failure_eof.get_err(), ~"Could not match string \"ab\" because EOI.");
 	}
 
 	#[test]
@@ -326,11 +387,11 @@ mod test {
 
 		let failure_nomatch = anyOf(~['a', 'b']).run_string("def");
 
-		assert_eq!(failure_nomatch, Err(~"Could not find matching char. found: 'd' required any of: ~['a', 'b']"));
+		assert_eq!(failure_nomatch.get_err(), ~"Could not find matching char. found: 'd' required any of: ~['a', 'b']");
 
 		let failure_eof = anyOf(~['a', 'b']).run_string("");
 
-		assert_eq!(failure_eof, Err(~"Could not match any of ~['a', 'b'] because EOI."));
+		assert_eq!(failure_eof.get_err(), ~"Could not match any of ~['a', 'b'] because EOI.");
 	}
 
 	#[test]
@@ -342,11 +403,49 @@ mod test {
 
 		let failure_nomatch = or(chr('a'), chr('b')).run_string("def");
 
-		assert_eq!(failure_nomatch, Err(~"Neither of the parsers matched: \nCould not find matching char. found: 'd' required: 'a'\nCould not find matching char. found: 'd' required: 'b'"));
+		assert_eq!(failure_nomatch.get_err(), ~"Neither of the parsers matched: \nCould not find matching char. found: 'd' required: 'a'\nCould not find matching char. found: 'd' required: 'b'");
 
 		let (res, rest) = or(chr('a'), success()).run_string("def").unwrap();
 
 		assert_eq!(res, Right(()));
 		assert_eq!(rest.to_str(), ~"def");
+	}
+
+	#[test]
+	fn test_map(){
+		let (txt, rest) = text(~"ab").run_string("abc").map(|t| *t + " MAPPED").unwrap();
+
+		assert_eq!(txt, ~"ab MAPPED");
+		assert_eq!(rest.to_str(), ~"c");
+
+		let p1 = map(text(~"12"), |txt| from_str::<int>(txt));
+		let (number, rest) = p1.run_string("12abc").unwrap();
+
+		assert_eq!(number, Some(12));
+		assert_eq!(rest.to_str(), ~"abc");
+
+		let p2 = map(text(~"23"), |txt| from_str::<int>(txt));
+		let failure_nomatch = p2.run_string("12abc");
+
+		assert_eq!(failure_nomatch.get_err(), ~"Could not find matching string. found: \"12\" required: \"23\"");
+	}
+
+	#[test]
+	fn test_flat_map(){
+		let p1 = flatMap(text(~"12"), |txt| match from_str::<int>(txt) { Some(*) => chr('a'), None => chr('b') });
+		let (character, rest) = p1.run_string("12abc").unwrap();
+
+		assert_eq!(character, 'a');
+		assert_eq!(rest.to_str(), ~"bc");
+
+		let failure_nomatch = p1.run_string("23abc");
+
+		assert_eq!(failure_nomatch.get_err(), ~"Could not find matching string. found: \"23\" required: \"12\"");
+
+		let p2 = flatMap(text(~"cd"), |txt| match from_str::<int>(txt) { Some(*) => chr('a'), None => chr('b') });
+		let (character, rest) = p2.run_string("cdba").unwrap();
+
+		assert_eq!(character, 'b');
+		assert_eq!(rest.to_str(), ~"a");
 	}
 }
